b11357286e03e5bdaa1ef3d1cacfc0c817e2fa00
Adrian <adrjak.aj@gmail.com>
2015-09-17 21:13:40 +0200

    oppgave 2 går opp :D

--- src/Oppgave2/CardGame.java
+++ src/Oppgave2/CardGame.java
@@ -1,15 +1,10 @@
 package Oppgave2;
-import java.util.ArrayDeque;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class CardGame {
 
-    private ArrayDeque<String> numbers;
-    private ArrayDeque<String> operators;
-    private List<String> list;
-    private StringTokenizer tokens;
-
+    private ArrayDeque<Integer> numbers;
+    private ArrayDeque<Character> operators;
 
     public CardGame() {
         numbers = new ArrayDeque<>();
@@ -18,63 +13,53 @@ public class CardGame {
 
     public int evaluateExpression(String expression) {
         int sum = 0;
-        String bobbleSum;
-        if(expression.indexOf('(')!=-1){
+        if (expression.indexOf('(') != -1) {
             int lastIndexOfBobble = expression.lastIndexOf(')');
             int firstIndexOfBobble = expression.indexOf('(');
-            String expressionWithoutBobble;
-            String expressionInBobble = expression.substring(firstIndexOfBobble+1, lastIndexOfBobble);
-            bobbleSum = evaluateExpression(expressionInBobble) + "";
-            expressionWithoutBobble = expression.substring(0, firstIndexOfBobble) + bobbleSum + expression.substring(lastIndexOfBobble+1, expression.length());
+            String expressionInBobble = expression.substring(firstIndexOfBobble + 1, lastIndexOfBobble);
+            String bobbleSum = evaluateExpression(expressionInBobble) + "";
+            String expressionWithoutBobble = expression.substring(0, firstIndexOfBobble) + bobbleSum + expression.substring(lastIndexOfBobble + 1, expression.length());
             sum = scanAndProcessTokens(expressionWithoutBobble);
-        }else{
+        } else {
             sum = scanAndProcessTokens(expression);
         }
         return sum;
     }
 
     public int scanAndProcessTokens(String expression) {
-        tokens = new StringTokenizer(expression, "+-/*%", true);
-        int sum = 0;
-        boolean checkIfItStartsWithOperator = false;
+        StringTokenizer tokens = new StringTokenizer(expression, "+-/*%", true);
+        int sum;
         while (tokens.hasMoreTokens()) {
             String nextToken = tokens.nextToken().trim();
             if (nextToken.length() == 0) {
                 continue;
             } else if (nextToken.equals("*") || nextToken.equals("/")) {
-                sum = doMath(nextToken, Integer.parseInt(numbers.pop()), Integer.parseInt(tokens.nextToken()));
-            }
-            else if (nextToken.equals("+") || nextToken.equals("-")){
-                operators.addLast(nextToken);
-            }
-            else {
-                numbers.addLast(nextToken);
+                numbers.addLast( doMath(nextToken.charAt(0), numbers.pop(), Integer.parseInt(tokens.nextToken().trim())));
+            } else if (nextToken.equals("+") || nextToken.equals("-")) {
+                operators.addLast(nextToken.charAt(0));
+            } else {
+                numbers.addLast(Integer.parseInt(nextToken));
             }
         }
-
-        while (numbers.size()!=0){
-
-                String mathSymbol = operators.pop();
-                if (mathSymbol.equals("+")) {
-                    sum = doMath("+", sum, Integer.parseInt(numbers.pop()));
-                } else if (mathSymbol.equals("-")) {
-                    sum = doMath("-", sum, Integer.parseInt(numbers.pop()));
-                }
+        sum = numbers.pop();
+        while (numbers.size() != 0) {
+            sum = doMath(operators.pop(), sum, numbers.pop());
         }
         return sum;
     }
 
-    public int doMath(String operator, int operand1, int operand2) {
-        if (operator.equals("+")) {
-            return operand1 +operand2;
-        } else if (operator.equals("-")) {
-            return operand1 - operand2;
-        } else if (operator.equals("*")) {
-            return operand1 * operand2;
-        } else if (operator.equals("/")) {
-            return operand1 / operand2;
-        }
+    public int doMath(char operator, int leftOperand, int rightOperand) {
 
-        return 0;
+
+        if (operator=='+') {
+            return leftOperand + rightOperand;
+        } else if (operator=='-') {
+            return leftOperand - rightOperand;
+        } else if (operator=='*') {
+            return leftOperand * rightOperand;
+        } else if (operator=='/') {
+            return leftOperand / rightOperand;
+        }
+        throw new InputMismatchException();
     }
 }
