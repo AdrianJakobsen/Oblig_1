0768289fc7d07448b0aa5a6a21f19c5e5e67cb5f
adrian <adrjak.aj@gmail.com>
2015-10-13 12:10:41 +0200

    fixed oppgave en tester

--- Tests/Oppgave1/MinReturnedTest.java
+++ Tests/Oppgave1/MinReturnedTest.java
@@ -10,111 +10,102 @@ import static org.junit.Assert.*;
 public class MinReturnedTest {
 
     MinReturned comperer;
-    Integer[] oneElementInIntArray;
-    Integer[] twoElementInIntArray;
-    Integer[] sevenElemetInIntArray;
-    String[] OneElementInStringArray;
-    String[] TwoElementInStringArray;
-    String[] sevenElementInStringArray;
-    Double[] oneElementInDoubleArray;
-    Double[] twoElementInDoulbeArray;
-    Double[] sevenElementInDoubleArray;
-    String[] emptyStringArray;
-    Integer[] arrayWithoutLenght;
-    Double[] arrayWithNoInitializedElements;
+
 
     @Before
     public void setUp() {
         comperer = new MinReturned();
-        oneElementInIntArray = new Integer[]{2};
-        twoElementInIntArray = new Integer[]{7, 4};
-        sevenElemetInIntArray = new Integer[]{19, 2, 9, 5, 1, 6, 14};
-        OneElementInStringArray = new String[]{"bob"};
-        TwoElementInStringArray = new String[]{"Alfred", "carl"};
-        sevenElementInStringArray = new String[]{"michael", "jim", "magnus", "thomas", "adrian", "mads", "frode"};
-        oneElementInDoubleArray = new Double[]{2.7};
-        twoElementInDoulbeArray = new Double[]{14.6, 12.0};
-        sevenElementInDoubleArray = new Double[]{5.6,8.6,9.0,6.4,9.4,4.6,10.4};
-        emptyStringArray = null;
-        arrayWithoutLenght = new Integer[0];
-        arrayWithNoInitializedElements = new Double[5];
     }
 
     @Test
     public void min_ReturnSmallestIntegerInAnArrayWithOnlyOneInteger_Two() {
+        Integer[] oneElementInIntArray = new Integer[]{2};
         int returnedValue = comperer.min(oneElementInIntArray);
         assertEquals(2, returnedValue);
     }
 
     @Test
     public void min_ReturnSmallestIntegerInAnArrayWithTwoIntegers_4() {
+        Integer[] twoElementInIntArray = new Integer[]{7, 4};
+
         int returnedValue = comperer.min(twoElementInIntArray);
         assertEquals(4, returnedValue);
     }
 
     @Test
     public void min_ReturnSmallestIntegerInAnArrayWithSevenIntegers() {
+        Integer[] sevenElemetInIntArray = new Integer[]{19, 2, 9, 5, 1, 6, 14};
         int returnedValue = comperer.min(sevenElemetInIntArray);
         assertEquals(1, returnedValue);
     }
 
     @Test
     public void min_ReturnedSmallestStringInAnArrayWithOnlyOneString_bob() {
+        String[] OneElementInStringArray = new String[]{"bob"};
         String returnedValue = comperer.min(OneElementInStringArray);
         assertEquals("bob", returnedValue);
     }
 
     @Test
     public void min_ReturnedSmallestStringInAnArrayWithTwoElementsInString_Alfred() {
+        String[] TwoElementInStringArray = new String[]{"Alfred", "carl"};
         String returnedValue = comperer.min(TwoElementInStringArray);
         assertEquals("Alfred", returnedValue);
     }
 
     @Test
     public void min_ReturnedSmallestStringInAnArrayWithSevenElementsInString_adrian() {
+        String[] sevenElementInStringArray = new String[]{"michael", "jim", "magnus", "thomas", "adrian", "mads", "frode"};
         String returnedValue = comperer.min(sevenElementInStringArray);
         assertEquals("adrian", returnedValue);
     }
 
     @Test
     public void min_ReturnedSmallestDoubleInAnDoubleArrayWithOnlyOneElement_TwoCommaSeven() {
+        Double[] oneElementInDoubleArray = new Double[]{2.7};
         double returnedValue = comperer.min(oneElementInDoubleArray);
         assertEquals(2.7, returnedValue, 0);
     }
 
     @Test
-    public void min_ReturnedSmallestDoubleInAnDoubleArrayWithTwoElements_TwelveCommaZero(){
+    public void min_ReturnedSmallestDoubleInAnDoubleArrayWithTwoElements_TwelveCommaZero() {
+        Double[] twoElementInDoulbeArray = new Double[]{14.6, 12.0};
         double returnedValue = comperer.min(twoElementInDoulbeArray);
         assertEquals(12.0, returnedValue, 0);
     }
 
     @Test
-    public void min_ReturnedSmallesDoubleInAnDoubleArrayWithSevenElements_FourCommaSix(){
+    public void min_ReturnedSmallesDoubleInAnDoubleArrayWithSevenElements_FourCommaSix() {
+        Double[] sevenElementInDoubleArray = new Double[]{5.6, 8.6, 9.0, 6.4, 9.4, 4.6, 10.4};
         double returnedValue = comperer.min(sevenElementInDoubleArray);
         assertEquals(4.6, returnedValue, 0);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void min_WhenSendingInAnNullArrayReturnWithexception_IlligalArgumentException(){
+    public void min_WhenSendingInAnNullArrayReturnWithexception_IlligalArgumentException() {
+        String[] emptyStringArray = null;
         String expectedException = comperer.min(emptyStringArray);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void min_WhenSendingInAnArrayWithoutAnyLengthReturnWithException_IlligalArgumentException(){
+    public void min_WhenSendingInAnArrayWithoutAnyLengthReturnWithException_IlligalArgumentException() {
+        Integer[] arrayWithoutLenght = new Integer[0];
         int expectedException = comperer.min(arrayWithoutLenght);
     }
 
     @Test(expected = NullPointerException.class)
-    public void min_WhenSendingInAnArrayWithoutAnyInitializedElementsReturnWithException_IlligalArgumentException(){
+    public void min_WhenSendingInAnArrayWithoutAnyInitializedElementsReturnWithException_IlligalArgumentException() {
+        Double[] arrayWithNoInitializedElements = new Double[5];
         double expectedException = comperer.min(arrayWithNoInitializedElements);
     }
+
     @Test
-    public void min_CircleOneSmallerThanCircleTwo_MinusOne(){
+    public void min_CircleOneSmallerThanCircleTwo_MinusOne() {
         Circle circle1 = new Circle(2.5);
         Circle circle2 = new Circle(5);
-        Circle[] arrayOfCirles = new Circle[]{circle1,circle2};
+        Circle[] arrayOfCirles = new Circle[]{circle1, circle2};
         double returnedValue = comperer.min(arrayOfCirles).radius;
-        assertEquals(2.5, returnedValue,0);
+        assertEquals(2.5, returnedValue, 0);
     }
 
 }
